5f8ecc4fc79a57e18fde695323521d11c0c8c44b
Nafis Sazid <nafissazid48@gmail.com>
2022-05-01 01:17:45 +0600

    added total amount raised in campaigns api responses

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -4,13 +4,13 @@ class CampaignsController < ApplicationController
   def index
     @campaigns = Campaign.all
 
-    render json: {status: :successful, payload: @campaigns}, except: [:total_amount_raised]
+    render json: {status: :successful, payload: @campaigns}
   end
 
   def filter_campaigns_by_sector
     @campaigns = Campaign.where(sector: params[:sector_name])
 
-    render json: {status: :successful, payload: @campaigns}, except: [:total_amount_raised]
+    render json: {status: :successful, payload: @campaigns}
   end
 
   private
